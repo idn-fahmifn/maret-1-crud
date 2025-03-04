3e8442fbfc2e48c64e45503d85c14dfbce2e73ab
fahmifn.idn@gmail.com <fahmifn.idn@gmail>
2025-03-04 16:38:36 +0700

    2.9 detail edit

--- resources/views/kategori/detail.blade.php
+++ resources/views/kategori/detail.blade.php
@@ -12,7 +12,7 @@
                     <h3 class="text-md font-semibold text-gray-900 dark:text-gray-200">{{$data->nama_kategori}}</h3>
                     <span class="text-sm">Detail kategori {{$data->nama_kategori}}</span>
                 </div>
-                <div class="mb-6 px-6">
+                <div class="overflow-x-auto mb-6 px-6">
                     <table class="min-w-full bg-white dark:bg-gray-800">
                         <tr>
                             <td class="dark:text-white">{{$data->nama_kategori}}</td>
@@ -25,19 +25,18 @@
             </div>
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <h3 class="text-md font-semibold text-gray-900 dark:text-gray-200">{{$data->nama_kategori}}</h3>
-                    <span class="text-sm">Detail kategori {{$data->nama_kategori}}</span>
+                    <h3 class="text-md font-semibold text-gray-900 dark:text-gray-200">Edit Kategori {{$data->nama_kategori}}</h3>
                 </div>
                 <div class="mb-6 px-6">
                     <form action="" method="post" enctype="multipart/form-data">
                         @csrf
                         <div class="mt-4">
                             <label class="block w-full text-gray-800 dark:text-gray-200">Nama Kategori</label>
-                            <input type="text" name="nama_kategori" required class="block w-full bg-white dark:bg-transparent text-gray-800 mt-1 dark:text-gray-200 rounded-md">
+                            <input type="text" name="nama_kategori" value="{{$data->nama_kategori}}" required class="block w-full bg-white dark:bg-transparent text-gray-800 mt-1 dark:text-gray-200 rounded-md">
                         </div>
                         <div class="mt-4">
                             <label class="block w-full text-gray-800 dark:text-gray-200">Thumbnail</label>
-                            <input type="file" name="thumbnail" required class="block w-full bg-white dark:bg-transparent text-gray-800 mt-1 dark:text-gray-200 rounded-md">
+                            <input type="file" name="thumbnail" value="{{$data->thumbnail}}" class="block w-full bg-white dark:bg-transparent text-gray-800 mt-1 dark:text-gray-200 rounded-md">
                         </div>
                         <div class="mt-4">
                             <button type="submit" class="bg-red-500 hover:bg-red-300 text-white rounded-md px-6 py-2">Tambah</button>
