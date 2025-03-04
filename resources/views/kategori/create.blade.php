13b63a854fbc9643dba0417ad3ba8e451fbb51fe
fahmifn.idn@gmail.com <fahmifn.idn@gmail>
2025-03-04 14:32:36 +0700

    2.4 button

--- resources/views/kategori/create.blade.php
+++ resources/views/kategori/create.blade.php
@@ -9,20 +9,23 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <h3 class="text-md font-semibold text-gray-200">Tambah Kategori</h3>
+                    <h3 class="text-md font-semibold text-gray-900 dark:text-gray-200">Tambah Kategori</h3>
                     <span class="text-sm">Form menambahkan kategori produk</span>
                 </div>
 
                 <div class="py-6 px-6">
                     <form action="" method="post">
                         @csrf
-                        <div class="mt-4 px-6">
+                        <div class="mt-4">
                             <label class="block w-full text-gray-800 dark:text-gray-200">Nama Kategori</label>
-                            <input type="text" name="nama_kategori" required class="block w-full bg-transparent text-gray-800 dark:text-gray-200 rounded-md">
+                            <input type="text" name="nama_kategori" required class="block w-full bg-white dark:bg-transparent text-gray-800 mt-1 dark:text-gray-200 rounded-md">
                         </div>
-                        <div class="mt-4 px-6">
+                        <div class="mt-4">
                             <label class="block w-full text-gray-800 dark:text-gray-200">Thumbnail</label>
-                            <input type="file" name="thumbnail" required class="block w-full bg-transparent text-gray-800 dark:text-gray-200 rounded-md">
+                            <input type="file" name="thumbnail" required class="block w-full bg-white dark:bg-transparent text-gray-800 mt-1 dark:text-gray-200 rounded-md">
+                        </div>
+                        <div class="mt-4">
+                            <button type="submit" class="bg-red-500 text-white px-6 py-2">Tambah</button>
                         </div>
                     </form>
                 </div>
