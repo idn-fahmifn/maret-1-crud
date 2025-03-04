92ad772dec8076bd2561e7a896b8e3544a636435
fahmifn.idn@gmail.com <fahmifn.idn@gmail>
2025-03-04 15:05:39 +0700

    2.6 upload file

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class KategoriController extends Controller
@@ -19,6 +20,17 @@ class KategoriController extends Controller
     public function store(Request $request)
     {
         $data = $request->all();
+        if($request->hasFile('thumbnail'))
+        {
+            $gambar = $request->file('thumbnail'); //mengambil gambar yang diupload di form.
+            $path = 'public/images/kategori'; //path menyimpan data gambar.
+            $ext = $gambar->getClientOriginalExtension(); //mengambil extension gambar yang asli.
+            $name = 'gambar_kategori_'.Carbon::now()->format('YmdHis').'.'.$ext; ///nama ketika sudah diupload.
+            $gambar->storeAs($path, $name); //disimpan ke path yang sudah ditentukan dengan nama file yang sudah ditentukan (gambar_kategori_tanggal.ext)
+            $data['thumbnail'] = $name; //nilai yang disimpan ke dalam database.
+        }
+
         return $data;
+
     }
 }
